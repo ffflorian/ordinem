c38de18fd625b9debb4b4fbcac1e1ac38943edf6
Florian Keller <ffflorian@users.noreply.github.com>
2019-03-16 18:54:48 +0100

    feat: Add file route (#4)

---
+++ src/util.ts
@@ -0,0 +1,35 @@
+import * as logdown from 'logdown';
+import * as moment from 'moment';
+
+class ExtendedLogger {
+  private readonly logger: logdown.Logger;
+
+  constructor(postfix: string) {
+    this.logger = logdown(`ordinem/${postfix}`, {
+      logger: console,
+      markdown: false,
+    });
+  }
+
+  debug(...args: any[]) {
+    this.logger.debug(`[${formatDate()}]`, ...args);
+  }
+
+  error(...args: any[]) {
+    this.logger.error(`[${formatDate()}]`, ...args);
+  }
+
+  info(...args: any[]) {
+    this.logger.info(`[${formatDate()}]`, ...args);
+  }
+
+  warn(...args: any[]) {
+    this.logger.warn(`[${formatDate()}]`, ...args);
+  }
+}
+
+function formatDate(): string {
+  return moment().format('YYYY-MM-DD HH:mm:ss');
+}
+
+export {ExtendedLogger, formatDate};
